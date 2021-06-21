51f7249fb8e933689dda794484471e5623d985f7
dmytro <tedi4t@gmail.com>
2021-06-21 18:49:17 +0300

    add changes to initial_data

--- backend/database/initial_data.sql
+++ backend/database/initial_data.sql
@@ -1,44 +1,52 @@
 INSERT INTO client VALUES (
-  "Дмитро",
-  "Урин",
-  "+380667962610",
-  "м. Луцьк, вул. Кравчука 48"
+  default,
+  'Дмитро',
+  'Урин',
+  '+380667962610',
+  'м. Луцьк, вул. Кравчука 48'
 );
 
 INSERT INTO good_type VALUES (
-  "салат"
+  default,
+  'салат'
 );
 
-CREATE TABLE IF NOT EXISTS hall (
-  "Sky Garden",
-  "тут якийсь опис"
+INSERT INTO hall VALUES (
+  default,
+  'Sky Garden',
+  'тут якийсь опис'
 );
 
-CREATE TABLE IF NOT EXISTS good (
+INSERT INTO good VALUES (
+  default,
   1,
-  "Грецький салат"
-  "одна зі страв, що асоціюється із грецькою кухнею",
+  'Грецький салат',
+  'одна зі страв, що асоціюється із грецькою кухнею',
   250,
   90,
-  "/salats/greek_salat.png"
+  '/salats/greek_salat.png'
 );
 
-CREATE TABLE IF NOT EXISTS order_ (
+INSERT INTO order_ VALUES (
+  default,
   1
 );
 
-CREATE TABLE IF NOT EXISTS good_hall (
+INSERT INTO good_hall VALUES (
+  default,
   1,
   1
 );
 
-CREATE TABLE IF NOT EXISTS order_element (
+INSERT INTO order_element VALUES (
+  default,
   1,
   1,
   10
 );
 
-CREATE TABLE IF NOT EXISTS sale (
+INSERT INTO sale VALUES (
+  default,
   1,
   70
 );
