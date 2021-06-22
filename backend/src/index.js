1bc804e4fd38d93a133a91e1528adadc741a447c
dmytro <tedi4t@gmail.com>
2021-06-22 18:22:50 +0300

    add main server file

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors')
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+app.use(express.json({ limit: '50mb' }));
+app.use(helmet());
+app.use(cors({ origin: '*' }));
+
+app.get('/', (req, res) => {
+  res.json('Seccessfully connected to voting system');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
