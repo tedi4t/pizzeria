7f0794612917027eb634255d730e227e22f52f36
dmytro <tedi4t@gmail.com>
2021-06-24 16:18:32 +0300

    add route for getting all gods in certain hall

--- backend/src/routes/good.js
+++ backend/src/routes/good.js
@@ -13,6 +13,14 @@ router.post('/add', async (req, res) => {
   res.status(200).json(added);
 });
 
+router.get('/hall/:hall_id', async (req, res) => {
+  const { hall_id } = req.params;
+  const queryAll = queries['Good.hallGoods'];
+  const paramsAll = [hall_id];
+  const got = (await pool.query(queryAll, paramsAll)).rows;
+  res.status(200).json(JSON.stringify(got));
+})
+
 router.get('/all', async (req, res) => {
   const queryAll = queries['Good.all'];
   const paramsAll = [];
