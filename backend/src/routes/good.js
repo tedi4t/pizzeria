c682835603568652ef8a15997e89c0326911a9b8
dmytro <tedi4t@gmail.com>
2021-06-24 15:17:46 +0300

    changes in route for adding good

--- backend/src/routes/good.js
+++ backend/src/routes/good.js
@@ -5,12 +5,12 @@ const router = express.Router();
 const pool = require('../db/database');
 const queries = require('../db/queries.json');
 
-router.post('/create', async (req, res) => {
+router.post('/add', async (req, res) => {
   const { typeID, name, description, weight, price, photo } = req.body;
-  const queryCreate = queries['Good.create'];
-  const paramsCreate = [typeID, name, description, weight, price, photo];
-  const created = (await pool.query(queryCreate, paramsCreate)).rows[0].id;
-  res.status(200).json(created);
+  const queryAdd = queries['Good.create'];
+  const paramsAdd = [typeID, name, description, weight, price, photo];
+  const added = (await pool.query(queryAdd, paramsAdd)).rows[0].id;
+  res.status(200).json(added);
 });
 
 module.exports = router;
