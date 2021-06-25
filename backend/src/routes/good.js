f1514231c98c7a5c51bbae5060e1029d7b51e84f
dmytro <tedi4t@gmail.com>
2021-06-25 13:46:39 +0300

    changes routes order

--- backend/src/routes/good.js
+++ backend/src/routes/good.js
@@ -5,13 +5,21 @@ const router = express.Router();
 const pool = require('../db/database');
 const queries = require('../db/queries.json');
 
-router.post('/add', async (req, res) => {
+router.post('/create', async (req, res) => {
   const { typeID, name, description, weight, price, photo } = req.body;
   const queryAdd = queries['Good.create'];
   const paramsAdd = [typeID, name, description, weight, price, photo];
   const added = (await pool.query(queryAdd, paramsAdd)).rows;
   res.status(200).json(added);
 });
+
+router.get('/hall/:hall_id/type/:type_id', async (req, res) => {
+  const { type_id, hall_id } = req.params;
+  const queryAll = queries['Good.typeHallGoods'];
+  const paramsAll = [type_id, hall_id];
+  const got = (await pool.query(queryAll, paramsAll)).rows;
+  res.status(200).json(JSON.stringify(got));
+})
 
 router.get('/hall/:hall_id', async (req, res) => {
   const { hall_id } = req.params;
@@ -25,14 +33,6 @@ router.get('/type/:type_id', async (req, res) => {
   const { type_id } = req.params;
   const queryAll = queries['Good.typeGoods'];
   const paramsAll = [type_id];
-  const got = (await pool.query(queryAll, paramsAll)).rows;
-  res.status(200).json(JSON.stringify(got));
-})
-
-router.get('/hall/:hall_id/type/:type_id', async (req, res) => {
-  const { type_id, hall_id } = req.params;
-  const queryAll = queries['Good.typeHallGoods'];
-  const paramsAll = [type_id, hall_id];
   const got = (await pool.query(queryAll, paramsAll)).rows;
   res.status(200).json(JSON.stringify(got));
 })
