efe35ebb2d873c860546c45d4590f1306a87d470
dmytro <tedi4t@gmail.com>
2021-06-25 13:43:18 +0300

    add routes for good

--- backend/database/query.sql
+++ backend/database/query.sql
@@ -10,6 +10,10 @@ SELECT * FROM order_element INNER JOIN order_ ON order_.id=order_element.order_i
 
 SELECT * FROM good INNER JOIN good_type ON good.type_id=good_type.id WHERE good.type_id=1;
 
+-- To show all goods of certain type in hall
+
+SELECT * FROM good INNER JOIN good_type ON good.type_id=good_type.id INNER JOIN good_hall ON good.id=good_hall.good_id WHERE good.type_id=1 AND good_hall.hall_id=1;
+
 -- Show all orders
 
 SELECT client_id, order_id, good_id, quantity, type_id, good.name, description, weight, price, photo FROM client INNER JOIN order_ ON client.id=order_.client_id INNER JOIN order_element ON order_element.order_id=order_.id INNER JOIN good ON good.id=order_element.good_id;
--- backend/src/routes/good.js
+++ backend/src/routes/good.js
@@ -21,6 +21,22 @@ router.get('/hall/:hall_id', async (req, res) => {
   res.status(200).json(JSON.stringify(got));
 })
 
+router.get('/type/:type_id', async (req, res) => {
+  const { type_id } = req.params;
+  const queryAll = queries['Good.typeGoods'];
+  const paramsAll = [type_id];
+  const got = (await pool.query(queryAll, paramsAll)).rows;
+  res.status(200).json(JSON.stringify(got));
+})
+
+router.get('/hall/:hall_id/type/:type_id', async (req, res) => {
+  const { type_id, hall_id } = req.params;
+  const queryAll = queries['Good.typeHallGoods'];
+  const paramsAll = [type_id, hall_id];
+  const got = (await pool.query(queryAll, paramsAll)).rows;
+  res.status(200).json(JSON.stringify(got));
+})
+
 router.get('/all', async (req, res) => {
   const queryAll = queries['Good.all'];
   const paramsAll = [];
