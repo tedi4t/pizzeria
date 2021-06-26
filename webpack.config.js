69cb730f092a2e5b85a782ec79f53524b0c07fc1
dmytro <tedi4t@gmail.com>
2021-06-26 13:34:57 +0300

    improved wabpack congig file (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,50 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const mode = 'development';
+
+const cssLoaders = (extra = []) => ([
+  {
+    loader: MiniCssExtractPlugin.loader,
+    options: {
+      hmr: mode,
+      reloadAll: true,
+    },
+  },
+  'css-loader',
+  ...extra,
+])
 
 module.exports = {
-  entry: path.resolve(__dirname, './front/src/index.js'),
+  mode,
+  context: path.resolve(__dirname, 'front'),
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './front/public'),
-  }
+    path: path.resolve(__dirname, './public'),
+  },
+  module:{
+    rules:[
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        options:{
+          presets:["@babel/preset-env", "@babel/preset-react"]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders(['less-loader']),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders(['sass-loader']),
+      },
+    ]
 }
+} 
