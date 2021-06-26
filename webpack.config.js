cada36fdfcdfd65ea5097def47807702a45acd4a
dmytro <tedi4t@gmail.com>
2021-06-26 13:29:19 +0300

    delete webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,1 @@
-const path = require('path');
-
-require('./front/public')
-module.exports = {
-  entry: path.resolve(__dirname, './front/src/index.js'),
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './front/public'),
-  }
-} 
+ 
