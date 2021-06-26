b728a3eb93422a5de294f0e733622af2f54c4197
dmytro <tedi4t@gmail.com>
2021-06-26 13:31:24 +0300

    webpack config initial commit (#1)

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
- 
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, './front/src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './front/public'),
+  }
+}
