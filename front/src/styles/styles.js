c720cec9b1dcd254161ed6cc6655867847c445ed
dmytro <tedi4t@gmail.com>
2021-06-26 19:18:40 +0300

    add styles

---
+++ front/src/styles/styles.js
@@ -0,0 +1,15 @@
+import './bootstrap-4.1.2/popper.js'
+import './bootstrap-4.1.2/bootstrap.min.css';
+
+import './about.css';
+import './about_responsive.css';
+import './blog.css';
+import './blog_responsive.css';
+import './contact.css';
+import './contact_responsive.css';
+import './elements.css';
+import './elements_responsive.css';
+import './main_styles.css';
+import './menu.css';
+import './menu_responsive.css';
+import './responsive.css';
