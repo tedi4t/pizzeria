94b3fd8f19aa4c9bacde8d548277045c9cd76ab1
dmytro <tedi4t@gmail.com>
2021-06-29 20:51:42 +0300

    add menu good type

--- front/src/modules/pages/menu/components/goodTypes.js
+++ front/src/modules/pages/menu/components/goodTypes.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../../../hooks/useFetch.js';
+
+const TypeElement = ({ type, activeID }) => {
+  console.log(type);
+  const classList = `good-types-element ${type.id === activeID ? 'active' : ''}`;
+  return (
+    <h1 className={classList}>
+      {type.name}
+    </h1>
+  )
+}
 
 export default () => {
-  
+  const [{ response, isLoading, error }, doFetch] = useFetch('/good/type/all');
+  const [activeType, setActiveType] = useState(2);
+
+  useEffect(() => {
+    doFetch()
+  }, [])
 
   return (
-    <div id = "halls">
-      Good Types
+    <div id = "good-types">
+      <div className="menu-title">Menu</div>
+      <div className="good-types-elements">
+        {
+          response && response.map((type, id) => (
+            <TypeElement key = {id} type = {type} activeID = {activeType} />
+          ))
+        }
+      </div>
     </div>
   )
 }
--- front/src/styles/styles.js
+++ front/src/styles/styles.js
@@ -7,3 +7,4 @@ import './navbar.scss';
 import './menu_title.scss';
 import './menu_heading.scss';
 import './menu_halls.scss';
+import './menu_good_type.scss';
