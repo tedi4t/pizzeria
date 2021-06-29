579f696d88c9235041b44ff8f1554146aaaa0e93
dmytro <tedi4t@gmail.com>
2021-06-29 20:10:11 +0300

    front: add halls to menu

--- front/src/modules/pages/menu/components/halls.js
+++ front/src/modules/pages/menu/components/halls.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFetch from '../../../hooks/useFetch.js';
+
+const HallElement = ({ hall, activeID }) => {
+  console.log(hall.id, activeID);
+  const classList = `hall-element ${hall.id === activeID ? 'active' : ''}`;
+  return (
+    <h1 className={classList}>
+      { hall.name }
+    </h1>
+  )
+}
 
 export default () => {
-  
+  const [{ response, isLoading, error }, doFetch] = useFetch('/hall/all');
+  const [activeHall, setActiveHall] = useState(1);
+
+  useEffect(() => {
+    doFetch()
+  }, [])
+
+  console.log(response);
 
   return (
     <div id = "halls">
-      Halls
+      <div className="hall-title">
+        The Venue
+      </div>
+      <div className="hall-subtitle">
+        Hall
+      </div>
+      <div className="hall-elements">
+        {
+          response && response.map((hall, id) => (
+            <HallElement key = {id} hall={hall} activeID={activeHall}/>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- front/src/styles/styles.js
+++ front/src/styles/styles.js
@@ -6,3 +6,4 @@ import './carousel.scss';
 import './navbar.scss';
 import './menu_title.scss';
 import './menu_heading.scss';
+import './menu_halls.scss';
