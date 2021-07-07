904ff2216588a68953a397b417a0683d877a441e
dmytro <tedi4t@gmail.com>
2021-07-08 01:47:39 +0300

    changes to database config

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -5,9 +5,14 @@ const config = require('./config/database.js')
 const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT,
   PG_DATABASE, NODE_ENV, DATABASE_URL } = process.env;
 
-const devConfig = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
+const devConfig = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`
 const proConfig = DATABASE_URL;
 
-const pool = new Pool(config);
+const pool = new Pool({
+  connectionString: NODE_ENV === 'production' ? devConfig : proConfig,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = pool;
