ceff01470cab8c6f419730eadd0edb2848418464
dmytro <tedi4t@gmail.com>
2021-07-08 02:14:43 +0300

    changed config for database

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,15 +1,19 @@
 'use strict';
 const { Pool } = require('pg');
-require('dotenv').config();
 
 const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT,
   PG_DATABASE, NODE_ENV, DATABASE_URL } = process.env;
 
-const devConfig = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`;
+const devConfig = `postgresql://${PG_USER}:${PG_PASSWORD}@${PG_HOST}:${PG_PORT}/${PG_DATABASE}`
 const proConfig = DATABASE_URL;
 
-const pool = new Pool({
-  connectionString: NODE_ENV === 'production' ? proConfig : devConfig
-});
+const config = {
+  connectionString: NODE_ENV === 'production' ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false
+  }
+};
+
+const pool = new Pool(config);
 
 module.exports = pool;
