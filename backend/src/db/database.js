6648544d0a663d012197063ee2e04e846db44bbb
dmytro <tedi4t@gmail.com>
2021-07-08 02:03:17 +0300

    fixing

--- backend/src/db/database.js
+++ backend/src/db/database.js
@@ -1,6 +1,5 @@
 'use strict';
 const { Pool } = require('pg');
-const config = require('./config/database.js')
 
 const { PG_USER, PG_PASSWORD, PG_HOST, PG_PORT,
   PG_DATABASE, NODE_ENV, DATABASE_URL } = process.env;
